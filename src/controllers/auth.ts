70a373d6e018f2258d9b6d6ea02f6d280669b22a
Marco Francisco de Oliveira Junior <mj991982@outlook.com>
2024-05-28 19:34:27 -0300

    creating and activating the login route

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,5 +1,16 @@
 import { RequestHandler, Request, Response } from "express";
+import { z } from "zod";
+
+import * as Auth from '../services/auth';
 
 export const login: RequestHandler = (req: Request, res: Response) => {
+    const loginSchema = z.object({
+        password: z.string()
+    });
+    const body = loginSchema.safeParse(req.body);
+    if (!body.success) return res.json({ error: 'Dados inválidos!!!' });
 
+    if (!Auth.validatePassword(body.data.password)) return res.status(403).json({ error: 'Acesso negado' });
+
+    res.json({ token: Auth.createToken() });
 }
