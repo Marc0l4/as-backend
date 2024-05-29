91e9cde2307b68177151aaeff3d88b4f47adcce1
Marco Francisco de Oliveira Junior <mj991982@outlook.com>
2024-05-29 14:37:18 -0300

    create middlewere validate for admin routes

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,4 +1,4 @@
-import { RequestHandler, Request, Response } from "express";
+import { RequestHandler, Request, Response, NextFunction } from "express";
 import { z } from "zod";
 
 import * as Auth from '../services/auth';
@@ -13,4 +13,17 @@ export const login: RequestHandler = (req: Request, res: Response) => {
     if (!Auth.validatePassword(body.data.password)) return res.status(403).json({ error: 'Acesso negado' });
 
     res.json({ token: Auth.createToken() });
+}
+
+export const validate: RequestHandler = (req: Request, res: Response, next: NextFunction) => {
+    if (!req.headers.authorization) {
+        return res.status(403).json({ error: 'Acesso negado' });
+    }
+
+    const token = req.headers.authorization.split(' ')[1];
+    if (!Auth.validateToken(token)) {
+        return res.status(403).json({ error: 'Acesso negado, Token invalido' });
+    }
+
+    next();
 }
--- src/routes/admin.ts
+++ src/routes/admin.ts
@@ -3,7 +3,7 @@ import * as Auth from '../controllers/auth';
 
 const router = Router();
 
-router.get('/ping', (req: Request, res: Response) => res.json({ pong: true }));
+router.get('/ping', Auth.validate, (req: Request, res: Response) => res.json({ pong: true, admin: true }));
 router.post('/login', Auth.login);
 
 export default router;
