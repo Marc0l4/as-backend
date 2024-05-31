25dd5fdc99a3a1d1cce41ae9ed3199cec882a51d
Marco Francisco de Oliveira Junior <mj991982@outlook.com>
2024-05-31 17:45:40 -0300

    create files and routes to list groups for an event

---
+++ src/controllers/groups.ts
@@ -0,0 +1,12 @@
+import { RequestHandler, Response, Request } from "express";
+
+import * as groups from '../services/groups';
+
+export const getAll: RequestHandler = async (req: Request, res: Response) => {
+    const { id_event } = req.params;
+
+    const items = await groups.getAll(parseInt(id_event));
+    if (items) return res.json({ groups: items });
+
+    res.json({ error: 'Ocorreu um erro' });
+}
--- src/routes/admin.ts
+++ src/routes/admin.ts
@@ -1,6 +1,8 @@
 import { Router, Request, Response } from "express";
+
 import * as Auth from '../controllers/auth';
 import * as Events from '../controllers/events';
+import * as Groups from '../controllers/groups';
 
 const router = Router();
 
@@ -13,5 +15,7 @@ router.get('/events/:id', Auth.validate, Events.getEvent);
 router.post('/events', Auth.validate, Events.addEvent);
 router.put('/events/:id', Auth.validate, Events.updateEvent);
 router.delete('/events/:id', Auth.validate, Events.deleteEvent);
+
+router.get('/events/:id_event/groups', Auth.validate, Groups.getAll);
 
 export default router;
