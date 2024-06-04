011b05c170ae2930fc5a77fbe316a75a5758faa3
Marco Francisco de Oliveira Junior <mj991982@outlook.com>
2024-06-04 16:34:25 -0300

    create folders & routes to acess peolpe in a group

---
+++ src/controllers/people.ts
@@ -0,0 +1,15 @@
+import { Request, Response, RequestHandler } from 'express';
+
+import * as people from '../services/people';
+
+export const getAll: RequestHandler = async (req: Request, res: Response) => {
+    const { id_event, id_group } = req.params;
+
+    const items = await people.getAll({
+        id_event: parseInt(id_event),
+        id_group: parseInt(id_group)
+    });
+    if (items) return res.json({ people: items });
+
+    res.json({ error: 'Ocorreu um erro' });
+}
--- src/routes/admin.ts
+++ src/routes/admin.ts
@@ -3,6 +3,7 @@ import { Router, Request, Response } from "express";
 import * as Auth from '../controllers/auth';
 import * as Events from '../controllers/events';
 import * as Groups from '../controllers/groups';
+import * as People from '../controllers/people';
 
 const router = Router();
 
@@ -21,5 +22,11 @@ router.get('/events/:id_event/groups/:id', Auth.validate, Groups.getGroup);
 router.post('/events/:id_event/groups', Auth.validate, Groups.addGroup);
 router.put('/events/:id_event/groups/:id', Auth.validate, Groups.updateGroup);
 router.delete('/events/:id_event/groups/:id', Auth.validate, Groups.deleteGroup);
+
+router.get('/events/:id_event/groups/:id_group/people', Auth.validate, People.getAll);
+/*router.get('/events/:id_event/groups/:id_group/people/:id', Auth.validate, People.getPeople);
+router.post('/events/:id_event/groups/:id_group/people', Auth.validate, People.addPeople);
+router.put('/events/:id_event/groups/:id_group/people/:id', Auth.validate, People.updatePeople);
+router.delete('/events/:id_event/groups/:id_group/people/:id', Auth.validate, People.deletePeople);*/
 
 export default router;
