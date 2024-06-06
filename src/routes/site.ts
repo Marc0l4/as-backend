2b985361e702320cdd9f0a2e57451b574a404899
Marco Francisco de Oliveira Junior <mj991982@outlook.com>
2024-06-06 20:08:27 -0300

    create route to user consult who took it

--- src/routes/site.ts
+++ src/routes/site.ts
@@ -1,11 +1,13 @@
 import { Router, Request, Response } from "express";
 
 import * as Events from '../controllers/events';
+import * as People from '../controllers/people';
 
 const router = Router();
 
 router.get('/ping', (req: Request, res: Response) => res.json({ pong: true }));
 
 router.get('/events/:id', Events.getEvent);
+router.get('/events/:id_event/search', People.searchPerson);
 
 export default router;
