4689790abc851fc30d5f2bf23934853c9b8e77b5
Marco Francisco de Oliveira Junior <mj991982@outlook.com>
2024-06-06 19:48:28 -0300

    create route to user verify to event is avaliabe

--- src/routes/site.ts
+++ src/routes/site.ts
@@ -1,7 +1,11 @@
 import { Router, Request, Response } from "express";
 
+import * as Events from '../controllers/events';
+
 const router = Router();
 
 router.get('/ping', (req: Request, res: Response) => res.json({ pong: true }));
+
+router.get('/events/:id', Events.getEvent);
 
 export default router;
